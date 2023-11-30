47e1b64f2f157fb675d3c572c6177d408c93c32a
vasil.storchak <git@github.com:Who-are-me/home_work_amazinum.git>
2023-11-30 16:56:36 +0200

    first commit

--- main.py
+++ main.py
@@ -1,126 +1,112 @@
 from PIL import Image
+import pandas as pd
+import time
+import os
 import requests
 from datasets import load_dataset
 from transformers import AutoProcessor, AutoModelForZeroShotImageClassification
 
 
-_NAMES = [
-    "apple_pie",
-    "baby_back_ribs",
-    "baklava",
-    "beef_carpaccio",
-    "beef_tartare",
-    "beet_salad",
-    "beignets",
-    "bibimbap",
-    "bread_pudding",
-    "breakfast_burrito",
-    "bruschetta",
-    "caesar_salad",
-    "cannoli",
-    "caprese_salad",
-    "carrot_cake",
-    "ceviche",
-    "cheesecake",
-    "cheese_plate",
-    "chicken_curry",
-    "chicken_quesadilla",
-    "chicken_wings",
-    "chocolate_cake",
-    "chocolate_mousse",
-    "churros",
-    "clam_chowder",
-    "club_sandwich",
-    "crab_cakes",
-    "creme_brulee",
-    "croque_madame",
-    "cup_cakes",
-    "deviled_eggs",
-    "donuts",
-    "dumplings",
-    "edamame",
-    "eggs_benedict",
-    "escargots",
-    "falafel",
-    "filet_mignon",
-    "fish_and_chips",
-    "foie_gras",
-    "french_fries",
-    "french_onion_soup",
-    "french_toast",
-    "fried_calamari",
-    "fried_rice",
-    "frozen_yogurt",
-    "garlic_bread",
-    "gnocchi",
-    "greek_salad",
-    "grilled_cheese_sandwich",
-    "grilled_salmon",
-    "guacamole",
-    "gyoza",
-    "hamburger",
-    "hot_and_sour_soup",
-    "hot_dog",
-    "huevos_rancheros",
-    "hummus",
-    "ice_cream",
-    "lasagna",
-    "lobster_bisque",
-    "lobster_roll_sandwich",
-    "macaroni_and_cheese",
-    "macarons",
-    "miso_soup",
-    "mussels",
-    "nachos",
-    "omelette",
-    "onion_rings",
-    "oysters",
-    "pad_thai",
-    "paella",
-    "pancakes",
-    "panna_cotta",
-    "peking_duck",
-    "pho",
-    "pizza",
-    "pork_chop",
-    "poutine",
-    "prime_rib",
-    "pulled_pork_sandwich",
-    "ramen",
-    "ravioli",
-    "red_velvet_cake",
-    "risotto",
-    "samosa",
-    "sashimi",
-    "scallops",
-    "seaweed_salad",
-    "shrimp_and_grits",
-    "spaghetti_bolognese",
-    "spaghetti_carbonara",
-    "spring_rolls",
-    "steak",
-    "strawberry_shortcake",
-    "sushi",
-    "tacos",
-    "takoyaki",
-    "tiramisu",
-    "tuna_tartare",
-    "waffles",
-]
+# print time of program
+def ptp(start_time, msg: str = ''):
+    print("Time:", msg, time.time() - start_time)
 
 
 if __name__ == '__main__':
-    # dataset = load_dataset('food101')
-    # print(dataset.values())
-    # train, validation
-    # print(dataset['train'][0])
+    st = time.time()
+
+    # load dataset
+    # list -> train, validation
+    dataset = load_dataset('food101')
+    ptp(st, 'load dataset')
+
+    df_names = pd.read_csv('names_of_food.csv')
+    ptp(st, 'load csv of names')
+    names = list()
+
+    for item in df_names['name']:
+        names.append(item)
+
+    processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch32")
+    ptp(st, 'load processor ai')
+
+    model = AutoModelForZeroShotImageClassification.from_pretrained("openai/clip-vit-base-patch32")
+    ptp(st, 'load model ai')
+
+    count_true_train = 0
+    count_false_train = 0
+
+    # for 'train' data
+    # item -> {'image': _, 'label': _}
+    for item in dataset['train']:
+        inputs = processor(
+            text=names,
+            images=item['image'],
+            return_tensors="pt",
+            padding=True
+        )
+
+        outputs = model(**inputs)
+        logits_per_image = outputs.logits_per_image
+        probs = logits_per_image.softmax(dim=1)
+
+        lprobs = probs.detach().numpy().tolist()
+
+        if lprobs.index(max(probs)) == item['label']:
+            count_true_train += 1
+        else:
+            count_false_train += 1
+
+        # print(outputs)
+        # print(logits_per_image)
+        # print(probs.detach().numpy()[0])
+
+        pass
+
+    count_true_validation = 0
+    count_false_validation = 0
+
+    # for 'validation' data
+    # item -> {'image': _, 'label': _}
+    for item in dataset['validation']:
+        inputs = processor(
+            text=names,
+            images=item['image'],
+            return_tensors="pt",
+            padding=True
+        )
+
+        outputs = model(**inputs)
+        logits_per_image = outputs.logits_per_image
+        probs = logits_per_image.softmax(dim=1)
+
+        lprobs = probs.detach().numpy().tolist()
+
+        if lprobs.index(max(probs)) == item['label']:
+            count_true_train += 1
+        else:
+            count_false_train += 1
+        pass
+
+    # print(len(dataset['train']))
+    #
+    # print("Time: ", time.time() - tm)
+    #im =
+    # dataset['train'][0]['image'].save('image.jpeg', format='jpeg')
+    # print("Time: ", time.time() - tm)
+    # print("Image")
+    # print("Time: ", time.time() - tm)
+    # print(im)
+
+    # im.save('image.jpeg', format='jpeg')
+
     # print(dataset.column_names)
     # print(_NAMES[dataset['train'][987].get('label')])
+
     # print(dataset.class_encode_column('label') )
     # print(type(dataset))
 
-    # processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch32")
-    # model = AutoModelForZeroShotImageClassification.from_pretrained("openai/clip-vit-base-patch32")
+
 
     # url = "http://images.cocodataset.org/val2017/000000039769.jpg"
     # image = Image.open(requests.get(url, stream=True).raw)
