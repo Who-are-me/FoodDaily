cf7c920beda1a0eafc8ff401351fd01b3a5c7a32
vasil.storchak <git@github.com:Who-are-me/home_work_amazinum.git>
2023-12-28 15:39:34 +0200

    some refactors

--- visualization.py
+++ visualization.py
@@ -13,8 +13,8 @@ y_pred = list()
 y_true_v = list()
 y_pred_v = list()
 
-df_true = pd.read_csv('true.csv')
-df_pred = pd.read_csv('pred.csv')
+df_true = pd.read_csv('validation_true.csv')
+df_pred = pd.read_csv('validation_pred.csv')
 
 df_true_v = df_true.iloc[750:]
 df_pred_v = df_pred.iloc[750:]
@@ -26,26 +26,26 @@ df_pred = df_pred.iloc[:750]
 for ind in df_true.index:
     y_true.extend(df_true.iloc[ind, df_true.columns != 'item'].values.tolist())
 
-for ind in range(0, 250):
-    y_true_v.extend(df_true_v.iloc[ind, df_true_v.columns != 'item'].values.tolist())
+# for ind in range(0, 250):
+#     y_true_v.extend(df_true_v.iloc[ind, df_true_v.columns != 'item'].values.tolist())
 
 # pred
 for ind in df_pred.index:
     y_pred.extend(df_pred.iloc[ind, df_pred.columns != 'item'].values.tolist())
 
-for ind in range(0, 250):
-    y_pred_v.extend(df_pred_v.iloc[ind, df_pred_v.columns != 'item'].values.tolist())
+# for ind in range(0, 250):
+#     y_pred_v.extend(df_pred_v.iloc[ind, df_pred_v.columns != 'item'].values.tolist())
 
 cls_names = [item for item in names if item in y_pred]
-cls_names_v = [item for item in names if item in y_pred_v]
+# cls_names_v = [item for item in names if item in y_pred_v]
 
-report_train = classification_report(y_true, y_pred, target_names=cls_names, output_dict=True)
-report_validation = classification_report(y_true_v, y_pred_v, target_names=cls_names_v, output_dict=True)
+# report_train = classification_report(y_true, y_pred, target_names=cls_names, output_dict=True)
+report_validation = classification_report(y_true, y_pred, target_names=cls_names, output_dict=True)
 
-df_report_train = pd.DataFrame(report_train).transpose()
+# df_report_train = pd.DataFrame(report_train).transpose()
 df_report_validation = pd.DataFrame(report_validation).transpose()
 
-df_report_train.to_csv('report_train.csv')
+# df_report_train.to_csv('report_train.csv')
 # df_report_train = df_report_train.sort_values(by=['precision'])
 
 df_report_validation.to_csv('report_validation.csv')
@@ -57,9 +57,10 @@ df_report_validation = df_report_validation.sort_values(by=['f1-score'])
 df_report_validation.to_csv('report_validation_sort_by_f1-score.csv')
 
 print("#" * 70)
-print("METRICS: train data")
+print("METRICS: validation data")
+# print("METRICS: train data")
 print(classification_report(y_true, y_pred, target_names=cls_names))
 
-print("#" * 70)
-print("METRICS: validation data")
-print(classification_report(y_true_v, y_pred_v, target_names=cls_names_v))
+# print("#" * 70)
+# print("METRICS: validation data")
+# print(classification_report(y_true_v, y_pred_v, target_names=cls_names_v))
